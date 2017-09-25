0556e13e414465e28c81f97f4ef99f7d620d0557
Kevin Coyner <rustybear@gmail.com>
2017-09-25 13:47:19 -0400

    Corrected writing function to Firebase.

--- routes/calls.js
+++ routes/calls.js
@@ -9,9 +9,11 @@ const firebaseApp = firebase.initializeApp(dbconfig);
 // get a test dispatch that already exists in the test_dispatch collection
 router.get('/', function(req, res, next) {
   var call = 1700034281;
-  firebaseApp.database().ref('/test_dispatches/' + call).once('value').then(function(snapshot) {
-    console.log(snapshot.val());
-    res.send(snapshot.val());
+  firebaseApp.database().ref('/test_d/' + call).once('value').then(function(snapshot) {
+    if (snapshot){
+      console.log(snapshot.val());
+      res.send(snapshot.val());
+    }
   });
 });
 
@@ -23,13 +25,16 @@ router.post('/', function(req, res, next) {
   let myQuery = JSON.stringify(req.query);
   // let myQuery = JSON.parse(req.query);
 
-  var newCallKey = firebaseApp.database().ref().child('/test_dispatches/').push().key;
-  req.query['callId'] = newCallKey;
-  var updateCalls = {};
-  updateCalls['/test_dispatches/'] = req.query;
-  firebaseApp.database().ref().update(updateCalls);
+  var newCall= firebaseApp.database().ref('/test_d').push(req.query);
+  var newKey = newCall.getKey();
 
-  res.send(`You POSTed: ${myQuery}`);
+
+  // req.query['callId'] = newCallKey;
+  // var updateCalls = {};
+  // updateCalls['/test_dispatches/'] = req.query;
+  // firebaseApp.database().ref().update(updateCalls);
+
+  res.send(`You POSTed: ${myQuery} ${newKey}`);
 
 
 });
