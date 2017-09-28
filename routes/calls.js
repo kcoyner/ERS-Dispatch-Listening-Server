a3eb80881cd4f4d345cb38e6d186aecdc67b7047
Kevin Coyner <rustybear@gmail.com>
2017-09-27 23:18:25 -0400

    Cleanup of code and added error trapping for POST.

--- routes/calls.js
+++ routes/calls.js
@@ -3,23 +3,24 @@ var router = express.Router();
 var dbconfig = require('../util/db-config');
 var firebase = require('firebase');
 require('firebase/database');
-const firebaseApp = firebase.initializeApp(dbconfig);
+// const firebaseApp = firebase.initializeApp(dbconfig);
 
 const tableName = "/ersDispatches/";
 
 /* GET calls listing. */
 // get a test dispatch that already exists in the ersDispatches collection
 router.get('/', function(req, res, next) {
-  var call = "KuuPfk1MgaO04MLKkId";
+  var call = "1";
   console.log('tableName: ', tableName);
   console.log('call: ', call);
-  firebaseApp.database().ref(tableName + call).once('value').then(function(snapshot) {
-    if (snapshot){
-      console.log(snapshot.val());
-      res.send(snapshot.val());
-    } else {
-      console.error('ERROR: failed to get a snapshot from Firebase');
-    }
+  firebase.database().ref(tableName + call).once('value')
+    .then(function(snapshot) {
+      if (snapshot){
+        console.log(snapshot.val());
+        res.send(snapshot.val());
+      } else {
+        console.error('ERROR: failed to get a snapshot from Firebase');
+      }
   });
 });
 
@@ -30,10 +31,15 @@ router.post('/', function(req, res, next) {
   console.log('req.query', req.query);
   let callQuery = JSON.stringify(req.query);
 
-  var newCall= firebaseApp.database().ref(tableName).push(req.query);
-  var newKey = newCall.getKey();
-
-  res.send(`You POSTed: ${callQuery}  ${newKey}`);
+  var newCall = firebase.database().ref(tableName).push(req.query, error => {
+    if (error) {
+      console.log(error);
+      res.sendStatus(500);
+    } else {
+      var newKey = newCall.getKey();
+      res.send(`Your POST: ${callQuery} and the new Key: ${newKey}`);
+    }
+  });
 });
 
 module.exports = router;
