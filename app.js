ee1bafc5b42e79dc4fcb46fdebe30630923100ce
Kevin Coyner <rustybear@gmail.com>
2017-11-29 21:55:00 -0500

    Add semantic-ui. Still need to make adjustments to webpack.config.js.

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/calls', calls)
+app.use('/calls', calls);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
